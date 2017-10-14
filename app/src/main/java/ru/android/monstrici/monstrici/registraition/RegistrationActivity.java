9df9246d2f3c561db661c2fc4c6fbc4c124c636a
Yasinaaa <yahootadaa@gmail.com>
2017-10-14 10:30:26 +0300

    init

---
+++ app/src/main/java/ru/android/monstrici/monstrici/registraition/RegistrationActivity.java
@@ -0,0 +1,8 @@
+package ru.android.monstrici.monstrici.registraition;
+
+/**
+ * Created by yasina on 13.10.17.
+ */
+
+public class RegistrationActivity {
+}
---
+++ app/src/main/java/ru/android/monstrici/monstrici/root/Application.java
@@ -0,0 +1,8 @@
+package ru.android.monstrici.monstrici.root;
+
+/**
+ * Created by yasina on 13.10.17.
+ */
+
+public class Application {
+}
